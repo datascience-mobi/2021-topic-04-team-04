5c642da942bc97ae17e7b8f8b8730ace2ac9b86e
LauraKaschnitz <laura0805@gmx.at>
2021-07-02 17:15:46 +0200

    unseeded region growing erste ergebnisse

--- Unseeded_speed_test.py
+++ Unseeded_speed_test.py
@@ -1,7 +1,7 @@
 
-from Functions import image_processing as ip
+#  from Functions import image_processing as ip
 import skimage.io as sk
-import numpy as np
+#  import numpy as np
 from Functions import unseeded_region_growing as urg
 from PIL import Image
 
@@ -12,11 +12,8 @@ image = sk.imread("Data/N2DH-GOWT1/img/t01.tif")
 #  img_small = image[0:500, 0:500]
 #  ip.show_image(img_small, 15, 8)
 
-regions = np.zeros(image.shape, int)  # array with region number
-regions[0, 0] = 1
 
-regions_test = regions.copy()
-img_result = urg.unseeded_region_growing_algorithm(image, regions_test, 5)
-if __name__ == '__main__':
-    im = Image.fromarray(img_result)
-    im.save("Result_Pictures/Unseeded_Region_Growing/N2DH-GOWT1/urg_t01.tif")
+img_result = urg.unseeded_region_growing_algorithm(image, (0, 0), 5)
+
+im = Image.fromarray(img_result)
+im.save("Result_Pictures/Unseeded_Region_Growing/N2DH-GOWT1/urg_t01.tif")
